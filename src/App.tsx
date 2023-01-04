a58f0fd2c2a9734028a8175e578952d6d878ba90
Rapidy <artemchik03211@gmail.com>
2023-01-05 02:19:35 +0500

    создал боковое меню

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import css from './App.module.scss';
 
-import { Counter } from './features/components/counter/Counter';
 import UserList from './features/components/UsersList/UserList';
-import CounterList from './features/components/CounterList/CounterList';
 
 import { users, counters } from './app/mocks/mocks';
 import { getUsers, setUsers } from './app/redux/slices/userSlice';
 import { getCounters, setCounters } from './app/redux/slices/counterSlice';
 import { useAppDispatch, useAppSelector } from './app/hooks';
+
+import Sidebar from './features/components/Sidebar/Sidebar';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -17,7 +17,7 @@ function App() {
   React.useEffect(() => {
     dispatch(setUsers(users));
     dispatch(setCounters(counters));
-  }, []);
+  }, [dispatch]);
 
   const userList = useAppSelector(getUsers);
   const counterList = useAppSelector(getCounters);
@@ -26,13 +26,13 @@ function App() {
     <div className={css.root}>
       <div className={css.container}>
         <div className={css.wrapper}>
-          <Routes location="/">
+          <Sidebar counters={counterList} />
+          <Routes>
             <Route
-              index
+              path="/:id"
               element={
                 <>
-                  <UserList users={userList} listName="Счетчик 1" />
-                  <CounterList counters={counterList} />
+                  <UserList users={userList} listName="Счетчик" />
                 </>
               }
             />
