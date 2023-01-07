dfb2cacb46e226aa12e2467d98036dddbbe33130
Artem Musevich <appleam015@gmail.com>
2023-01-07 16:27:08 +0500

    Revert "Layout перестановка "
    This reverts commit 5087759e420c39953885331b321fdb76530ed452.

--- src/features/components/Layout/Layout.tsx
+++ src/features/components/Layout/Layout.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import css from './Layout.module.scss';
-import { Outlet } from 'react-router-dom';
+import Sidebar from '../Sidebar/Sidebar';
 import { counters } from '../../../app/mocks/mocks';
 import {
   getCounters,
   setCounters
 } from '../../../app/redux/slices/counterSlice';
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
-
-import Sidebar from '../Sidebar/Sidebar';
-import Header from '../Header/Header';
+import { Outlet } from 'react-router-dom';
+import css from './Layout.module.scss';
 
 const Layout: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -23,9 +21,8 @@ const Layout: React.FC = () => {
   return (
     <div className={css.root}>
       <div className={css.container}>
-        <Sidebar counters={counterList} />
         <div className={css.wrapper}>
-          <Header />
+          <Sidebar counters={counterList} />
 
           <Outlet />
         </div>
