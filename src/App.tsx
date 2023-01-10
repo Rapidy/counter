87561076e0803ecc9fc949bb3510550563b971bd
AlexandrV <alexandrvweb@gmail.com>
2023-01-11 02:11:02 +0500

    Добавляем CounterLog в App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import UserList from './features/components/UsersList/UserList';
+import CounterLog from './features/components/CounterLog/CounterLog';
 
-import { users } from './app/mocks/mocks';
+import { users, logs } from './app/mocks/mocks';
 import { getUsers, setUsers } from './app/redux/slices/userSlice';
+import {
+  getCounterLog,
+  setCounterLog
+} from './app/redux/slices/counterLogSlice';
 import { useAppDispatch, useAppSelector } from './app/hooks';
 
 import Layout from './features/components/Layout/Layout';
@@ -14,16 +19,23 @@ function App() {
 
   React.useEffect(() => {
     dispatch(setUsers(users));
+    dispatch(setCounterLog(logs));
   }, [dispatch]);
 
   const userList = useAppSelector(getUsers);
+  const counterLog = useAppSelector(getCounterLog);
 
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route
           path="counter/:id"
-          element={<UserList users={userList} listName="Счетчик" />}
+          element={
+            <>
+              <UserList users={userList} listName="Счетчик" />{' '}
+              <CounterLog logs={counterLog} />
+            </>
+          }
         />
       </Route>
     </Routes>
