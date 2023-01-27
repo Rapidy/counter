0986a262667ec18e20bf32e1d0e7036d749a6148
AlexandrV <alexandrvweb@gmail.com>
2023-01-27 21:21:42 +0500

    Создание компонента PopUp

---
+++ src/features/components/PopUp/PopUp.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import css from './PopUp.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+interface Props {
+  children: React.ReactNode;
+  is: boolean;
+  toggle: () => void;
+  title?: string;
+}
+
+const PopUp: React.FC<Props> = ({ children, is, toggle, title }) => {
+  return (
+    <>
+      {is && (
+        <div className={css.popup}>
+          <div className={css.container}>
+            <div className={css.box}>
+              <button type="button" className={css.close} onClick={toggle}>
+                <FontAwesomeIcon icon={faXmark} />
+              </button>
+              {title && (
+                <div className={css.header}>
+                  <h3 className={css.title}>{title}</h3>
+                </div>
+              )}
+              <div className={css.content}>{children}</div>
+            </div>
+          </div>
+          <div className={css.overlay} onClick={toggle} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default PopUp;
