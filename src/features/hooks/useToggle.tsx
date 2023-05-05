c244359ba5a48402f4c3e49cc687e0fa2ee93ac5
Artem Musevich <appleam015@gmail.com>
2023-05-05 14:41:13 +0500

    useToggle переместил элементы кортежа

--- src/features/hooks/useToggle.tsx
+++ src/features/hooks/useToggle.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-type HookType = [boolean, (value: boolean) => void, () => void];
+type HookType = [boolean, () => void, (value: boolean) => void];
 
 const useToggle = (initial: boolean = false): HookType => {
   const [state, setState] = React.useState(initial);
 
   const toggle = () => setState(!state);
 
-  return [state, setState, toggle];
+  return [state, toggle, setState];
 };
 
 export default useToggle;
